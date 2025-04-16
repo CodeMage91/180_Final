8a16fc0608e86d8b22a2137ab13b96d749e5f600
Devin Ayala <167722328+CodeMage91@users.noreply.github.com>
2025-04-16 15:09:20 -0400

    Item added to test page

--- 180_FINAL/main.py
+++ 180_FINAL/main.py
@@ -19,11 +19,14 @@ def all_users():
                                       ).mappings().fetchall()
     customer_users = db.session.execute( text("select * from shop_user where user_type = 'Customer'")
                                         ).mappings().fetchall()
+    items = db.session.execute( text("select * from shop_item")
+                                  ).mappings().fetchall()
 
     return render_template('test.html',
                             admin_users=admin_users,
                             vendor_users=vendor_users, 
-                            customer_users=customer_users)
+                            customer_users=customer_users,
+                            items=items)
 
 #run#
 if __name__ == '__main__':
