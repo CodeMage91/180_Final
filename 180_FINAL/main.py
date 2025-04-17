90b444f9378646ef68e274a279ddd4fcef0f6ac9
Devin Ayala <167722328+CodeMage91@users.noreply.github.com>
2025-04-17 02:32:35 -0400

    create item function fixed

--- 180_FINAL/main.py
+++ 180_FINAL/main.py
@@ -11,52 +11,50 @@ db = SQLAlchemy(app)
 #routes#
 
 #test page to see everything!#
-@app.route('/', methods=['GET','POST'])
+@app.route('/', methods=['GET', 'POST'])
 def all_users():
-    admin_users = db.session.execute( text("select * from shop_user where user_type = 'Admin'")
-                                     ).mappings().fetchall()
-    vendor_users = db.session.execute( text("select * from shop_user where user_type = 'Vendor'")
-                                      ).mappings().fetchall()
-    customer_users = db.session.execute( text("select * from shop_user where user_type = 'Customer'")
-                                        ).mappings().fetchall()
-    items = db.session.execute( text("select * from shop_item")
-                                  ).mappings().fetchall()
-    creator = db.session.execute(text('select * from shop_user where user_id = 1'))
-    
-    #Create User#
+    admin_users = db.session.execute(text("SELECT * FROM shop_user WHERE user_type = 'Admin'")).mappings().fetchall()
+    vendor_users = db.session.execute(text("SELECT * FROM shop_user WHERE user_type = 'Vendor'")).mappings().fetchall()
+    customer_users = db.session.execute(text("SELECT * FROM shop_user WHERE user_type = 'Customer'")).mappings().fetchall()
+    items = db.session.execute(text("SELECT * FROM shop_item")).mappings().fetchall()
+    creator = db.session.execute(text("SELECT * FROM shop_user WHERE user_id = 1")).mappings().fetchone()
+
     if request.method == 'POST':
-        signup_data = {
-            'full_name':request.form['full_name'],
-            'email':request.form['email'],
-            'username':request.form['username'],
-            'password_hash':request.form['password_hash'],
-            'user_type':request.form['user_type']
-        }
-        db.session.execute(text("""
-        insert into shop_user(full_name,email,username,password_hash,user_type)
-                                values(:full_name,:email,:username,:password_hash,:user_type)
-        """), signup_data)
+        if 'full_name' in request.form:  # This means the Create User form was submitted
+            signup_data = {
+                'full_name': request.form['full_name'],
+                'email': request.form['email'],
+                'username': request.form['username'],
+                'password_hash': request.form['password_hash'],
+                'user_type': request.form['user_type']
+            }
+            db.session.execute(text("""
+                INSERT INTO shop_user (full_name, email, username, password_hash, user_type)
+                VALUES (:full_name, :email, :username, :password_hash, :user_type)
+            """), signup_data)
+            db.session.commit()
 
-        create_item = {
-            'item_name':request.form['item_name'],
-            'original_price':request.form['original_price'],
-            'item_desc':request.form['item_desc'],
-            'created_by':1
-        }
-        db.session.execute(text("""
-            insert into shop_item( item_name, original_price, item_desc, created_by)
-            values(:item_name, original_price, item_desc, created_by)    
-    """), create_item)
-
-        db.session.commit()
+        elif 'item_name' in request.form:  # This means the Create Item form was submitted
+            create_item = {
+                'item_name': request.form['item_name'],
+                'item_image':request.form['item_image'],
+                'original_price': request.form['original_price'],
+                'item_desc': request.form['item_desc'],
+                'created_by': 1
+            }
+            db.session.execute(text("""
+                INSERT INTO shop_item (item_name, item_image,original_price, item_desc, created_by)
+                VALUES (:item_name, :item_image, :original_price, :item_desc, :created_by)
+            """), create_item)
+            db.session.commit()
 
     return render_template('test.html',
-                            admin_users=admin_users,
-                            vendor_users=vendor_users, 
-                            customer_users=customer_users,
-                            items=items,
-                            creator=creator
-                            )
+                           admin_users=admin_users,
+                           vendor_users=vendor_users,
+                           customer_users=customer_users,
+                           items=items,
+                           creator=creator)
+
 
 #run#
 if __name__ == '__main__':
